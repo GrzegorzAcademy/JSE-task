d7a3e012b987ef2275ddbb2861491c9840fafd0d
GrzegorzAcademy <sparki_21@tlen.pl>
2024-03-25 16:51:54 +0100

    solusion task 1b

---
+++ src/Engine.java
@@ -0,0 +1,5 @@
+public class Engine {
+    Integer power;
+    Integer capacity;
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 
 public class Main {
     public static void main(String[] args) {
-
-        MyClass myClass1 = new MyClass();
-        myClass1.number = 1;
-        myClass1.text = "myClassText 1";
-        MyClass myClass2 = new MyClass();
-        myClass2.number = 2;
-        myClass2.text = "MyClass 2";
-        System.out.println(myClass1.number + " " + myClass1.text);
-        System.out.println(myClass2.number + " " + myClass2.text);
-
+        Engine engine1 = new Engine();
+        engine1.power = 100;
+        engine1.capacity = 1000;
+        Engine engine2 = new Engine();
+        engine2.power = 200;
+        engine2.capacity = 2000;
+        System.out.println("Engine 1 power " + engine1.power);
+        System.out.println("Engine 1 capacity " + engine1.capacity);
+        System.out.println("Engine 2 power " + engine2.power);
+        System.out.println("Engine 2 capacity " + engine2.capacity);
 
     }
 }
