1d648c163b3173aa8231cd5aba1e4db2da01afca
GrzegorzAcademy <sparki_21@tlen.pl>
2024-03-26 18:44:07 +0100

    solusion task 10

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("podaj liczbę całkowita, a ja powiem Ci czy liczba jest parzysta ");
-        int number = sc.nextInt();
-
-        isEvenSwitch(number);
-        isEven(number);
-
-    }
-
-    public static void isEvenSwitch(int number) {
-        switch (number % 2) {
-            case 0 -> System.out.println("liczba jest parzsta, podałeś : " + number);
-            case 1 -> System.out.println("Podana liczba jest liczba nieparzystą, podałeś :" + number);
-            default -> System.out.println(" Nieprawidłowa liczba, podałeś :" + number);
-        }
-    }
-
-    public static void isEven(int number) {
-        if (number % 2 == 0) {
-            System.out.println("Liczba jest parzysta, podałeś : " + number);
-        } else if (number % 2 == 1) {
-            System.out.println("Podana liczba jest niearzysta, podałeś :" + number);
-        } else {
-            System.out.println("Nieprawidłowa liczba, podałeś : " + number);
+        int number = 5;
+        while (number > 0) {
+            System.out.println(number);
+            number--;
         }
 
     }
