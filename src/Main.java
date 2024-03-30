fee47bf1721dfacd2b938e7311eb6941b3941259
GrzegorzAcademy <sparki_21@tlen.pl>
2024-03-30 07:00:15 +0100

    solusion task 13

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
-        int number = 10;
-        for (int i = 0; i < number; i++) {
-            System.out.println(i);
-        }
+        int i = 14;
+        liste(i);
+    }
 
+    private static void liste(int i) {
+        for (int j = 0; j <= i; j++) {
+            if (j % 2 == 0) {
+                continue;
+            }
+            if (j % 11 == 0) {
+                break;
+            }
+            System.out.println(j);
+        }
     }
 }
