9be25978e3dbb6e3c6b1767b58bf6de15e2d0a70
GrzegorzAcademy <sparki_21@tlen.pl>
2024-04-02 14:57:12 +0200

    solusion task 15

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Engine engine = new Engine();
         Scanner sc = new Scanner(System.in);
-        System.out.println("podaj pierwszą liczbę");
-        int number1 = sc.nextInt();
-        System.out.println("Podaj drugą liczbę");
-        int number2 = sc.nextInt();
-        System.out.println("wynik = " + (number1 - number2));
+        System.out.println("Podaj marke");
+        car1.setName(sc.nextLine());
+        System.out.println("podaj moc");
+        engine.setPower(sc.nextInt());
+        car1.setEngine(engine);
+        System.out.println("Car name " + car1.getName());
+        System.out.println("Engine Power");
+        car1.getEngine().getPowerCar();
+
     }
 }
