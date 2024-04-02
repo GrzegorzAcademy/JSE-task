023b7d2f4c99d5ed65f5c98b25919a68bf93a17e
GrzegorzAcademy <sparki_21@tlen.pl>
2024-04-02 15:26:07 +0200

    solusion task 16

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,14 @@
-import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Boolean check = true;
-        Scanner sc = new Scanner(System.in);
 
-        do {
-            System.out.println("POdaj liczbę");
-            int number = sc.nextInt();
-            if (number % 3 == 0) {
-                System.out.println("Wynik = " + number / 3);
-                check = false;
-            } else {
-                System.out.println("POdana liczba nie dzili sie przz 3 bez reszty");
-
-            }
-
-
-        }while (check) ;
+        String name1 = "Jan";
+        String name2 = "Jan";
+        System.out.println(name2 == name1);
+        System.out.println(name2.equals(name1));
+        System.out.println(name2.hashCode());
+        System.out.println(name1.hashCode());
     }
 }
