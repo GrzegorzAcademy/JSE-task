cda736d2dc7c3fdff1b6baa6b03d4c8734d36179
GrzegorzAcademy <sparki_21@tlen.pl>
2024-04-03 16:55:42 +0200

    solusion task 17

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj zdanie podzielone kropkami " +
+                " a powiem Ci ole kropek jest w zdaniu ");
+        String s = sc.nextLine();
+        char[] charArray = s.toCharArray();
 
-        String name1 = "Jan";
-        String name2 = "Jan";
-        System.out.println(name2 == name1);
-        System.out.println(name2.equals(name1));
-        System.out.println(name2.hashCode());
-        System.out.println(name1.hashCode());
+        int count = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == '.')
+                count++;
+        }
+        System.out.println(count);
     }
 }
