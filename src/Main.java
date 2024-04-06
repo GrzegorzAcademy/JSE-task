0fad11c82d976877d9e9c5e906113917a7fd1890
GrzegorzAcademy <sparki_21@tlen.pl>
2024-04-06 18:15:20 +0200

    solusion task P2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        Car car1 = new Car();
-        Engine engine = new Engine();
-        engine.setPower(2000);
-        car1.setName("Audi");
-        car1.setMaxSpeed(200);
-        car1.setEngine(engine);
-        car1.setColour(Colour.GREEN);
-        System.out.println("Auto " + car1.getName() + ", O mocy " + car1.getEngine().getPower() + " KW "
-                + ", Posiada kolor " + car1.getColour());
 
+        countEven(20);
+    }
+
+    public static void countEven(int x) {
+        int count = 1;
+        if (x > 0 && x % 2 != 0) {
+            for (int i = 0; i < x; i++) {
+                if (i % 5 == 0) continue;
+                count *= i;
+            }
+            System.out.println(count);
+        } else {
+            System.out.println("bład");
+        }
     }
 }
