e7cafbbee6ec679d727d3092e5b9381c30655aa6
GrzegorzAcademy <sparki_21@tlen.pl>
2024-04-07 19:14:44 +0200

    solusion task 22

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
-
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-Car car1 = new Car("BMW");
-car1.setColour(Colour.GREEN);
-        System.out.println(car1.toString());
+
+        Scanner sc = new Scanner(System.in);
+        double numberDouble = 0;
+        try {
+            System.out.println("Podaj liczbę ");
+            numberDouble = sc.nextDouble();
+            System.out.println("Podna wartość " + numberDouble);
+        } catch (InputMismatchException e) {
+            System.out.println("błędna wartość!");
+        }
+
 
     }
 }
