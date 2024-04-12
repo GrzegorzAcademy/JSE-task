2b935a780a9237f881cfb80e6eed14abd2c64cba
GrzegorzAcademy <sparki_21@tlen.pl>
2024-04-12 15:50:43 +0200

    solusion task 27

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,18 @@ import java.nio.file.StandardOpenOption;
 
 public class Main {
     public static void main(String[] args) {
-        String pathOfResources = "src/resources/plik.txt";
-        String value = "default value";
-        Path path = Path.of(pathOfResources);
-        try {
-            Files.writeString(path, value, StandardOpenOption.CREATE);
-        } catch (IOException e) {
-            System.out.println("wystapił błąd podczas zapisywania pliku");
+var namesfunction = getTable("ala","kasia");
+        for (int i = 0; i < namesfunction.length; i++) {
+            System.out.println(namesfunction[i]);
         }
-        try {
-            String content = Files.readString(path);
-            System.out.println(content);
-        } catch (IOException e) {
-            System.err.println("pład odczytu pliku ");
+    }
+
+    public static String[] getTable(String... names) {
+        String[] namesList= new String[names.length];
+        for (int i = 0; i < namesList.length; i++) {
+            namesList[i]=names[i];
         }
+return namesList;
     }
 }
 
